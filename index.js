3102a0d57cb3b0c844da1aca561a3c24d45f2297
Thomas Nkuna <thomasnkuna96@gmail.com>
2021-10-05 13:32:51 +0200

    using express

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 
 const express = require('express');
 const exphbs  = require('express-handlebars');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const SettingsBill = require('./settings-bill');
 
 const app = express();
-
 const settingsBill = SettingsBill();
 
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  partialsDir: "./views/partials",
+    viewPath:  './views',
+    layoutsDir : './views/layouts'
+}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 // parse application/json
-app.use(bodyParser.json())
+app.use(express.json())
 
 app.get("/", function(req, res){
-  res.render('index');
+  res.render('index', {
+    settings: settingsBill.getSettings()
+    });
 });
 
-app.post('/settings ', function(req,res){
+app.post('/settings', function(req,res){
 
-SettingsBill.setSettings({
-	callCost: req.body.callCost,
-	smsCost: req.body.smsCost,
-	warninglevel: req.body.warninglevel,
-	criticalLevel: req.body.criticalLevel,
+settingsBill.setSettings({
+  callCost: req.body.callCost,
+  smsCost: req.body.smsCost,
+  warningLevel: req.body.warningLevel,
+  criticalLevel: req.body.criticalLevel
 });
 
-console.log(SettingsBill.getSettings());
+console.log(settingsBill.getSettings());
+
 res.redirect('/');
 });
 
-app.post('/action ', function(req,res){
+app.post('/action', function(req,res){
 
 });
 
