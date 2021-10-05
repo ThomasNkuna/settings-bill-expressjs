01ca633d93739bbbb2b93016f230d46041191b38
Thomas Nkuna <thomasnkuna96@gmail.com>
2021-10-06 01:07:20 +0200

    working app

--- index.js
+++ index.js
@@ -52,11 +52,12 @@ app.post('/action', function(req,res){
 });
 
 app.get("/actions", function(req, res){
-	res.render('actions');
+	res.render('actions', {actions:settingsBill.actions()});
 });
 
-app.get("/actions/:type", function(req, res){
-	
+app.get("/actions/:actionType", function(req, res){
+  const actionType = req.params.actionType;
+  res.render('actions', {actions:settingsBill.actionsFor(actionType)});
 });
 
 
